4cf7c4b7bb1aa45e9ced92f9aa2a7e72c1fa4e0b
Nicolai Aamann <justplainme247@gmail.com>
2016-09-14 12:41:03 +0200

    MOAR HTML!
    Did you even read the title?

---
+++ Page/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>FnJ Apotek</title>
+  </head>
+  <body>
+    <div class="banner">
+      <h1>INSERT WORLD CLASS BANNER HERE</h1>
+    </div>
+    <div class="navbar">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Drugs</a></li>
+        <li><a href="index.php">Illegal meds</a></li>
+        <li><a href="index.php">Coke</a></li>
+        <li><a href="index.php">Heroin</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+    <div class="main">
+      <p><?php include('test_text.txt'); ?></p>
+    </div>
+  </body>
+</html>
